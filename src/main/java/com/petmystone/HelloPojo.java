c3ec30064ff5bba9f5260906f02bd2bf5f68f840
Timothy Stone <javafueled@gmail.com>
2016-07-30 18:35:19 -0400

    Compile for 1.8; correct formatter output.

--- src/main/java/com/petmystone/HelloPojo.java
+++ src/main/java/com/petmystone/HelloPojo.java
@@ -6,7 +6,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class HelloPojo implements RequestHandler<RequestClass, ResponseClass>{   
 
     public ResponseClass handleRequest(RequestClass request, Context context){
-        String greetingString = String.format("Hello %s, %s.", request.firstName, request.lastName);
+        String greetingString = String.format("Hello, %s %s.", request.firstName, request.lastName);
         return new ResponseClass(greetingString);
     }
 }
